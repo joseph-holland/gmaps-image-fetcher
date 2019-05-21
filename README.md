cf6814070db4f6dc5517626c780374bf652064e7
pp-joeholland <joe@phonepulse.ie>
2019-05-21 11:11:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# GMaps Image Fetcher
+
+Simple tool to download image tiles from Google Maps via the Static-Maps API
+
+## How it works
+
+* Give the tool two sets of latitudes and longitudes for the north-west and south-east of the area you wish to download
+the imagery from
+* The tool calculates how many individual image tiles it needs to get from Google's Static-Maps API and begins to
+download them
+* All image tiles are stitched together and a preview showed onscreen
+* The image is also saved to a file
+
+## Installation
+
+### Prerequisites
+
+* A GOOGLE_MAPS_API_KEY is required as per the T&Cs of Google's Static-Maps API (plenty of tutorials online showing you
+how to generate this, Google it)
+
+### Existing Python environment
+
+If you wish to download this into an existing Python environment you can us pip to install the tool like so:
+
+```bash
+pip install git+https://github.com/joseph-holland/gmaps-image-fetcher.git
+
+pip install -r requirements.txt
+```
+
+### Windows executable
+
+If on Windows platform I've compiled a release of the tool into an exe using [PyInstaller](https://www.pyinstaller.org/)
+
+Just download the latest release from the
+[Release section](https://github.com/joseph-holland/gmaps-image-fetcher/releases) of the repo
+
+## Running
+
+1. Set your GOOGLE_MAPS_API_KEY environment variable
+
+    Windows
+    ```bash
+    set GOOGLE_MAPS_API_KEY=AIzaS.............
+    ```
+    
+    Linux
+    ```bash
+    export GOOGLE_MAPS_API_KEY=AIzaS.............
+    ```
+    
+2. Run the tool and give it the NW lat/lon and SE lat/lon as well as a zoom level (1= world, 20=buildings)
+
+    ```bash
+    gmaps-image-fetcher.py -nw 53.369745 -6.348743 -se 53.348326 -6.296656 -z 16
+    ```
+
+3. The tool will calculate how many image tiles it will need to retrieve and prompt if you wish to continue
+
+    ```bash
+    Retrieve 15 image tiles from Google static-maps API
+    Do you want to continue y/n: y
+    ```
+
+4. It will then download the image tiles, stitch them together, show the image onscreen and save to the local directory
---
+++ gmaps-image-fetcher.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+"""
+Stitch together Google Maps images from lat, long coordinates
+
+Based on work by enigmaticPhysicist, heltonbiker and BenElgar on StackExchange
+https://stackoverflow.com/questions/7490491/capture-embedded-google-map-image-with-python-without-using-a-browser
+
+Changes:
+* Ability to set GOOGLE_MAPS_API_KEY as environment variable
+* Added command-line options for NW and SE Lat/Lons and zoom level
+* Added option to quit/continue (if the amoutn of images to be downloaded was too great)
+* Ability to save images
+* Added logging
+* Dramatically simplified the maths.
+* Set a more reasonable default logo cutoff.
+* Added global constants for logo cutoff and max image size.
+* Translated a couple presumably Portuguese variable names to English.
+* Updated for Python 3
+* Added Google Maps API key (compliance with T&C, although can set to None)
+* Handle http request exceptions
+"""
+
+import argparse
+import logging
+import sys
+from io import BytesIO
+from os import environ
+
+import requests
+from PIL import Image
+from math import log, exp, tan, atan, ceil
+from datetime import datetime
+
+__version__ = "0.3.2"
+
+# circumference/radius
+tau = 6.283185307179586
+# One degree in radians, i.e. in the units the machine uses to store angle,
+# which is always radians. For converting to and from degrees. See code for
+# usage demonstration.
+DEGREE = tau / 360
+
+ZOOM_OFFSET = 8
+
+# Max width or height of a single image grabbed from Google.
+MAXSIZE = 600
+# For cutting off the logos at the bottom of each of the grabbed images.  The
+# logo height in pixels is assumed to be less than this amount.
+LOGO_CUTOFF = 32
+
+
+def latlon_to_pixels(lat, lon, zoom):
+    mx = lon
+    my = log(tan((lat + tau / 4) / 2))
+    res = 2 ** (zoom + ZOOM_OFFSET) / tau
+    px = mx * res
+    py = my * res
+    return px, py
+
+
+def pixels_to_latlon(px, py, zoom):
+    res = 2 ** (zoom + ZOOM_OFFSET) / tau
+    mx = px / res
+    my = py / res
+    lon = mx
+    lat = 2 * atan(exp(my)) - tau / 4
+    return lat, lon
+
+
+def get_maps_image(nw_lat_long, se_lat_long, zoom=18):
+    try:
+        GOOGLE_MAPS_API_KEY = environ['GOOGLE_MAPS_API_KEY']  # set to 'your_API_key'
+    except KeyError as e:
+        logger.error("Please set your GOOGLE_MAPS_API_KEY environment variable")
+        sys.exit(1)
+
+    # Unpack lats/lons
+    ullat, ullon = nw_lat_long
+    lrlat, lrlon = se_lat_long
+
+    # convert all these coordinates to pixels
+    ulx, uly = latlon_to_pixels(ullat, ullon, zoom)
+    lrx, lry = latlon_to_pixels(lrlat, lrlon, zoom)
+
+    dx, dy = lrx - ulx, uly - lry  # Calculate total pixel dimensions of final image
+    cols, rows = ceil(dx / MAXSIZE), ceil(dy / MAXSIZE)  # Calculate rows and columns
+
+    logger.debug("GOOGLE_MAPS_API_KEY: ".format(GOOGLE_MAPS_API_KEY))
+
+    print("Retrieve {} image tiles from Google static-maps API".format(cols * rows))
+    user_input = input("Do you want to continue y/n: ")
+    if user_input == 'n':
+        sys.exit(0)
+    else:
+        # calculate pixel dimensions of each small image
+        width = ceil(dx / cols)
+        height = ceil(dy / rows)
+        heightplus = height + LOGO_CUTOFF
+
+        # assemble the image from stitched
+        final = Image.new('RGB', (int(dx), int(dy)))
+        for x in range(cols):
+            for y in range(rows):
+                dxn = width * (0.5 + x)
+                dyn = height * (0.5 + y)
+                latn, lonn = pixels_to_latlon(
+                    ulx + dxn, uly - dyn - LOGO_CUTOFF / 2, zoom)
+                position = ','.join((str(latn / DEGREE), str(lonn / DEGREE)))
+                logger.info("Getting image tile from column {0} row {1} for position {2}".format(x, y, position))
+                urlparams = {
+                    'center': position,
+                    'zoom': str(zoom),
+                    'size': '%dx%d' % (width, heightplus),
+                    'maptype': 'satellite',
+                    'sensor': 'false',
+                    'scale': 1
+                }
+                logger.debug("urlparams: {}".format(urlparams))
+                if GOOGLE_MAPS_API_KEY is not None:
+                    urlparams['key'] = GOOGLE_MAPS_API_KEY
+
+                url = 'http://maps.google.com/maps/api/staticmap'
+                try:
+                    response = requests.get(url, params=urlparams)
+                    response.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    print(e)
+                    sys.exit(1)
+
+                im = Image.open(BytesIO(response.content))
+                final.paste(im, (int(x * width), int(y * height)))
+
+        return final
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        # Prevent logging from propagating to the root logger
+        logger.propagate = 0
+        console = logging.StreamHandler()
+        logger.addHandler(console)
+        formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        console.setFormatter(formatter)
+    return logger
+
+
+if __name__ == "__main__":
+    # Configure command-line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
+    parser.add_argument('-d', '--debug', help='Enable debug output', action='store_true')
+    parser.add_argument('-l', '--logfile', help='Logfile to write', nargs=1)
+    parser.add_argument('-nw', '--northwest', help='NW Lat/Lon', nargs=2)
+    parser.add_argument('-se', '--southeast', help='SE Lat/Lon', nargs=2)
+    parser.add_argument('-z', '--zoom', help='Zoom level from 1 (world) to 20+ (buildings), default is 18', nargs=1)
+    options = parser.parse_args()
+
+    # Configure logging
+    logger = get_logger(__name__)
+    logger.setLevel(logging.INFO)
+    if options.logfile:
+        fh = logging.FileHandler(str(options.logfile))
+        logger.addHandler(fh)
+    if options.debug:
+        logger.setLevel(logging.DEBUG)
+        logger.debug("Command-line options: {}".format(options))
+
+    nw_lat_long = (float(options.northwest[0]) * DEGREE, float(options.northwest[1]) * DEGREE)
+    se_lat_long = (float(options.southeast[0]) * DEGREE, float(options.southeast[1]) * DEGREE)
+
+    # Get satellite image
+    result = get_maps_image(nw_lat_long, se_lat_long, zoom=int(options.zoom[0]))
+    result.show()  # Show onscreen
+    result.save("satellite_" + datetime.now().strftime('%Y%m%d_%H%M%S') + ".bmp")  # Save image
